e9cec6d7f5fe44483b589440cb4aeedc75e77a39
Tejas Mishr <113692600+TejasMishr@users.noreply.github.com>
2024-02-21 21:33:08 +0530

    Time: 7 ms (64.93%), Space: 8.8 MB (46.79%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div201-bitwise-and-of-numbers-range/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div201-bitwise-and-of-numbers-range.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div201-bitwise-and-of-numbers-range/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divadobe-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div201-bitwise-and-of-numbers-range.cpp
@@ -1,19 +1,27 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
-         int count=0;
-       // simple solution is to do bitwise and and return the sum. 
-      //     for(int i=m;i<=n;i++)
-      //               sum&=i;        
-		 //      return sum;
-        while(m!=n)  // see till both numbers are equal
-        {   // right shift both numbers by 1
-            m>>=1; 
-            n>>=1;
-            count++;  // increment the count.
-        }
-		//count gives the number of zero places from the lsb so left shift m by count.
-        return m<<count;
-    }
+    bitset<32> b1(m), b2(n), b3(0);
+    for(int i=31; i>=0; i--)
+        if(b1[i]==b2[i])
+            b3[i] = b1[i];
+        else break;
+    return b3.to_ulong();
+}
+		// int rangeBitwiseAnd(int m, int n) {
+		// int count=0;
+		// // simple solution is to do bitwise and and return the sum. 
+		// //     for(int i=m;i<=n;i++)
+		// //               sum&=i;        
+		//  //      return sum;
+		// while(m!=n)  // see till both numbers are equal
+		// {   // right shift both numbers by 1
+		// m>>=1; 
+		// n>>=1;
+		// count++;  // increment the count.
+		// }
+		// //count gives the number of zero places from the lsb so left shift m by count.
+		// return m<<count;
+		// }
 	
 };
