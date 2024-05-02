d7500bc5f22b0539eca87de128389cde9f608eee
Tejas Mishr <113692600+TejasMishr@users.noreply.github.com>
2024-05-02 18:08:40 +0530

    Time: 13 ms (84.90%), Space: 22.8 MB (79.57%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2441-largest-positive-integer-that-exists-with-its-negative.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     int findMaxK(vector<int>& nums) {
-        int maxi = -1;
-        for(int i=0; i< nums.size(); i++){
-            if(nums[i] > 0 && find(nums.begin(),nums.end(), -nums[i]) != nums.end()){
-                maxi = max(maxi,nums[i]);
-            }
+        // int maxi = -1;
+        // for(int i=0; i< nums.size(); i++){
+        //     if(nums[i] > 0 && find(nums.begin(),nums.end(), -nums[i]) != nums.end()){
+        //         maxi = max(maxi,nums[i]);
+        //     }
+        // }
+        // return maxi;
+
+        
+        // TC O(n⋅logn)  SC O(logn)
+        sort(nums.begin(),nums.end());
+        int low=0,high=nums.size()-1;
+        while(low<high){
+            if(nums[high] == -nums[low]) return nums[high];
+            
+            else if( -nums[low] > nums[high]) low++;
+            
+            else high--;
         }
-        return maxi;
+        return -1;
+        
     }
 };
