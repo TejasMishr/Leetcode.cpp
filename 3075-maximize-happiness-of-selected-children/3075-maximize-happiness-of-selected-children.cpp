51d8decb7b35c7622f44bbed3c8af916a479fbec
Tejas Mishr <113692600+TejasMishr@users.noreply.github.com>
2024-05-10 19:00:13 +0530

    Time: 219 ms (17.31%), Space: 116.7 MB (7.72%) - LeetHub

--- 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
+++ 3075-maximize-happiness-of-selected-children/3075-maximize-happiness-of-selected-children.cpp
@@ -1,14 +1,24 @@
 class Solution {
 public:
     long long maximumHappinessSum(vector<int>& arr, int k) {
-        sort(arr.begin(),arr.end(),greater<int>());
+        // sort(arr.begin(),arr.end(),greater<int>());
         long long tot=0;
         int turns=0;
+        // for(int i = 0; i < k; i++) {
+        //     tot+=max(arr[i]-turns,0);
+        //     turns++;
+        // }
+        // return tot;
+        
+        priority_queue<int> pq;
+        for(auto x:arr) pq.push(x);
         for(int i = 0; i < k; i++) {
-            tot+=max(arr[i]-turns,0);
+            tot+=max(pq.top()-turns,0);
+            pq.pop();
             turns++;
         }
         return tot;
+        
         
         
     }
