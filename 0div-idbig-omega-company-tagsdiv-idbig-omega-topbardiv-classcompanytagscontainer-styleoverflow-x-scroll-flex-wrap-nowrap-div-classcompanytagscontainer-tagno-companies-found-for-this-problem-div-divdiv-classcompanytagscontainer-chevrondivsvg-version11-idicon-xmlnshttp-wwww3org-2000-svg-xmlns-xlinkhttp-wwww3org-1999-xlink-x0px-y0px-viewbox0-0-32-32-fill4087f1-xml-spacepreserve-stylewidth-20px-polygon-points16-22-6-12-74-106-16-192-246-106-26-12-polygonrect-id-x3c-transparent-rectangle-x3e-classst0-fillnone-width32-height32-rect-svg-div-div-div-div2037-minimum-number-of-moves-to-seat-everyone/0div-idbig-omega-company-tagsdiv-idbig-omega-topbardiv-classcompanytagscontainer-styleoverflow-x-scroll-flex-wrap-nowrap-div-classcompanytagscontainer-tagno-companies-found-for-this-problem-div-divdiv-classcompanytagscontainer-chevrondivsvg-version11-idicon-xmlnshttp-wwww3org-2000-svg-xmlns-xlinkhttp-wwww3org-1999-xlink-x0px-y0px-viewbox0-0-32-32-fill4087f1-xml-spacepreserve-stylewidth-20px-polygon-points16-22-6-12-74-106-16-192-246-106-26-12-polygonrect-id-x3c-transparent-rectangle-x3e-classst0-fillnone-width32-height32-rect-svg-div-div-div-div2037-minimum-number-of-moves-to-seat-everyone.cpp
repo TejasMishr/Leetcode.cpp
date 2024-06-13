c39f44d3224c9dd62d83a682742abe8169cfdac4
Tejas Mishr <113692600+TejasMishr@users.noreply.github.com>
2024-06-13 13:31:22 +0530

    Time: 14 ms (6.67%), Space: 21 MB (52.06%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2037-minimum-number-of-moves-to-seat-everyone/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2037-minimum-number-of-moves-to-seat-everyone.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2037-minimum-number-of-moves-to-seat-everyone/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tagno-companies-found-for-this-problem-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,11 +1,29 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& std) {
-        sort(seats.begin(),seats.end());
-        sort(std.begin(),std.end());
-        int mov=0;
-        for(int i=0;i<seats.size();i++) mov+=abs(seats[i]-std[i]);
+        
+        //n logn
+        // sort(seats.begin(),seats.end());
+        // sort(std.begin(),std.end());
+        // int mov=0;
+        // for(int i=0;i<seats.size();i++) mov+=abs(seats[i]-std[i]);
+        // return mov;
+        
+        
+        int maxi1= *max_element(seats.begin(),seats.end());
+        int maxi2= *max_element(std.begin(),std.end());
+        int maxi= max(maxi1,maxi2);
+        
+        vector<int> diff(maxi+1,0);
+        for(int i=0; i<seats.size();i++) diff[seats[i]]++;
+        for(int j=0; j<std.size();j++) diff[std[j]]--;
+        
+        int mov=0, unmatched=0;
+        
+        for(auto x: diff) mov+=abs(unmatched), unmatched+=x;
+        
         return mov;
+        
     }
    
 };
